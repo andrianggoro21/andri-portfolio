54a0f6c6a381f74ca2f699c771c90a27c1f727fa
Andri Anggoro <andri.anggoro21@gmail.com>
2025-06-16 22:33:56 +0700

    fix: update about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,33 +3,31 @@ import Man from "../../assets/man.svg";
 const About = () => {
   return (
     <div
-      id="about"
-      className="bg-bg-color3 max-w-screen min-h-screen flex items-center justify-center "
+      id='about'
+      className='bg-bg-color3 max-w-screen min-h-screen flex items-center justify-center '
     >
-      <div className="w-4/5 h-full xl:h-500 flex flex-col-reverse xl:flex-row gap-12 xl:gap-0">
-        <div className="w-full xl:w-1/2 h-full flex flex-col justify-center gap-4">
-          <div className="hidden xl:block">
-            <span className="text-white font-raleway font-extrabold text-48">
+      <div className='w-4/5 h-full xl:h-500 flex flex-col-reverse xl:flex-row gap-12 xl:gap-0'>
+        <div className='w-full xl:w-1/2 h-full flex flex-col justify-center gap-4'>
+          <div className='hidden xl:block'>
+            <span className='text-white font-raleway font-extrabold text-48'>
               About
             </span>
           </div>
-          <p className="text-white font-raleway font-normal text-xl tracking-normal text-left">
-            An electrical engineering graduate committed to exploring the field
-            of software development with enthusiasm to expand knowledge and
-            skills in information technology. Possesses a strong understanding
-            of technical concepts and basic programming, along with a strong
-            desire to become a skilled software developer. Aiming to leverage
-            existing technical skills and problem-solving abilities to build a
-            successful career in software development. Ready to take on new
-            challenges in the world of technology with determination and
-            dedication.
+          <p className='text-white font-raleway font-normal text-xl tracking-normal text-left'>
+            A passionate and adaptable Software Developer with a strong
+            foundation in full-stack development. Experienced in designing and
+            implementing robust backend systems, crafting intuitive and
+            responsive user interfaces, and improving overall application
+            performance. Skilled in managing data flow, deploying applications
+            to cloud environments, and integrating with various services to
+            support complex system requirements.
           </p>
         </div>
-        <div className="w-full xl:w-1/2 h-full flex items-center justify-center xl:justify-end">
-          <img src={Man} alt="man" />
+        <div className='w-full xl:w-1/2 h-full flex items-center justify-center xl:justify-end'>
+          <img src={Man} alt='man' />
         </div>
-        <div className="flex xl:hidden justify-center">
-          <span className="text-white font-raleway font-extrabold text-48">
+        <div className='flex xl:hidden justify-center'>
+          <span className='text-white font-raleway font-extrabold text-48'>
             About
           </span>
         </div>
